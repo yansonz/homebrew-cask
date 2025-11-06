eb0c283012e0791fd8abfd0f64721360aa3a3ff1
얀_소성운 <yan.so@yan-soseong-un-ui-MacBookPro.local>
2025-11-06 11:18:54 +0900

    Update repository name to before-after-compare

--- Casks/before-after-compare.rb
+++ Casks/before-after-compare.rb
@@ -2,10 +2,10 @@ cask "before-after-compare" do
   version "1.0.0"
   sha256 "fd20a405a76fcef03d554024b1ee8d8ba46c00a8dda84f4c4375b0cbf85bc07c"
 
-  url "https://github.com/yansonz/before-and-after/releases/download/v#{version}/BeforeAfterCompare-#{version}.zip"
+  url "https://github.com/yansonz/before-after-compare/releases/download/v#{version}/BeforeAfterCompare-#{version}.zip"
   name "Before After Compare"
   desc "제품 개선 전후 비교 이미지를 쉽게 만들 수 있는 맥 앱"
-  homepage "https://github.com/yansonz/before-and-after"
+  homepage "https://github.com/yansonz/before-after-compare"
 
   app "BeforeAfterCompare.app"
 
